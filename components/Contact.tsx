925bfc7d00d34d62641ef7d27eaf9221c810196e
Neamul01 <neamulhaque2002@gmail.com>
2022-10-01 21:33:20 +0600

    contact me page added

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,7 +1,40 @@
-import { Box, Stack, Typography } from '@mui/material'
+import { Box, Card, CardContent, IconButton, Stack, Typography } from '@mui/material'
 import React from 'react'
+import EmailIcon from '@mui/icons-material/Email';
+import PhoneIcon from '@mui/icons-material/Phone';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 export const Contact = () => {
+    const contacts = [
+        {
+            email: 'neamulhaque2002@gmail.com',
+            icon: 'EmailIcon'
+        },
+        {
+            phone: '+8801865636932'
+        },
+        {
+            skype: 'hello'
+        },
+        {
+            facebook: 'fb'
+        },
+        {
+            linkedIn: 'linkedin'
+        },
+        {
+            whatsapp: 'whatsApp'
+        }
+    ]
+
+    // const MaterialIcon = ({ icon: string }) => {
+    //     switch (icon) {
+    //         case 'AccessAlarmIcon': return <EmailIcon />
+    //         default: return null
+    //     }
+    // }
     return (
         <Box
             sx={{
@@ -68,88 +101,152 @@ export const Contact = () => {
             </Stack>
             <Stack spacing={4}
                 sx={{
+                    my: "4rem",
+                    flexDirection: 'row',
+                    flexWrap: 'wrap',
                     alignItems: 'center',
-                    my: "4rem"
+                    justifyContent: 'center',
+                    gap: '1rem'
                 }}
             >
-                <Box sx={{
-                    // height: 500,
-                    width: "80%",
-                    // overflowY: 'scroll'
-                }}>
-                    {/* <ImageList
-                        cols={3}
-                        gap={16}
-                        sx={{
-                            cursor: 'pointer'
-                        }}>
 
-                        {/* latest 2 post */}
-                    {/* {
-                            latest2post.map((item, index) => (
-                                <Card
-                                    sx={{
-                                        bgcolor: 'background.paper',
-                                        height: 'auto'
-                                    }}
-                                    key={index}
-                                >
-                                    <LatestPost
-                                        item={item}
-                                    ></LatestPost>
-                                </Card>
-                            ))
-                        } */}
-
-                    {/* <ImageList
-                            cols={1}
-                            sx={{
-                                bgcolor: 'background.paper'
-                            }}
-                        >
-                            {/* another posts  */}
-                    {/* {
-                        restPosts.map((item, index) => (
-                            <Card
-                                sx={{
-                                    backgroundColor: 'background.default !important',
-                                    height: 'auto',
-                                    display: "flex",
-                                    alignItems: 'center',
-                                    backgroundImage: 'none'
-                                }}
-                                key={index}
-                            >
-                                <RestBlog
-                                    item={item}
-                                ></RestBlog>
-                            </Card>
-                        ))
-                    } */}
-                    {/* <Typography
+                {/* email card  */}
+                <Card sx={{ minWidth: 275 }}>
+                    <CardContent
                         sx={{
-                            textTransform: "initial",
-                            color: 'text.secondary',
-                            bgcolor: 'background.default',
-                            position: 'relative',
-                            fontWeight: 'bold',
-                            pt: '1.5rem'
+                            display: 'flex',
+                            alignItems: 'center',
+                            padding: '1.5rem'
                         }}
                     >
-                        All Blogs
-                        <NavigateNextIcon sx={{
-                            color: 'text.secondary',
-                            fontSize: '1.3rem',
-                            position: 'absolute',
-                            left: 87,
-                            bottom: 17,
-                            fontWeight: "900",
-                            // ml: 1
-                        }} />
-                    </Typography> */}
-                    {/* </ImageList> */}
-                    {/* </ImageList> */}
-                </Box>
+                        <IconButton aria-label="email">
+                            <EmailIcon />
+                        </IconButton>
+                        <Typography
+                            sx={{
+                                fontSize: '1.1rem',
+                                color: 'text.secondary',
+                                fontWeight: 'bold',
+                                ml: '1rem'
+                            }}
+                        >neamulhaque3298@gmail.com</Typography>
+                    </CardContent>
+                </Card>
+
+                {/* phone card  */}
+                <Card sx={{ minWidth: '22rem', marginTop: '0 !important' }}>
+                    <CardContent
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            padding: '1.5rem',
+                        }}
+                    >
+                        <IconButton aria-label="email">
+                            <PhoneIcon />
+                        </IconButton>
+                        <Typography
+                            sx={{
+                                fontSize: '1.1rem',
+                                color: 'text.secondary',
+                                fontWeight: 'bold',
+                                ml: '1rem'
+                            }}
+                        >+8801865636932</Typography>
+                    </CardContent>
+                </Card>
+
+                {/* skype card  */}
+                <Card sx={{ minWidth: 275, marginTop: '0 !important' }}>
+                    <CardContent
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            padding: '1.5rem'
+                        }}
+                    >
+                        <IconButton aria-label="email">
+                            <EmailIcon />
+                        </IconButton>
+                        <Typography
+                            sx={{
+                                fontSize: '1.1rem',
+                                color: 'text.secondary',
+                                fontWeight: 'bold',
+                                ml: '1rem'
+                            }}
+                        >neamulhaque3298@gmail.com</Typography>
+                    </CardContent>
+                </Card>
+
+                {/* facebook card  */}
+                <Card sx={{ minWidth: 275 }}>
+                    <CardContent
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            padding: '1.5rem'
+                        }}
+                    >
+                        <IconButton aria-label="email">
+                            <FacebookIcon />
+                        </IconButton>
+                        <Typography
+                            sx={{
+                                fontSize: '1.1rem',
+                                color: 'text.secondary',
+                                fontWeight: 'bold',
+                                ml: '1rem'
+                            }}
+                        >neamulhaque3298@gmail.com</Typography>
+                    </CardContent>
+                </Card>
+
+                {/* linkedin card  */}
+                <Card sx={{ minWidth: 275 }}>
+                    <CardContent
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            padding: '1.5rem'
+                        }}
+                    >
+                        <IconButton aria-label="email">
+                            <LinkedInIcon />
+                        </IconButton>
+                        <Typography
+                            sx={{
+                                fontSize: '1.1rem',
+                                color: 'text.secondary',
+                                fontWeight: 'bold',
+                                ml: '1rem'
+                            }}
+                        >neamulhaque3298@gmail.com</Typography>
+                    </CardContent>
+                </Card>
+
+                {/* whatsApp card  */}
+                <Card sx={{ minWidth: 275 }}>
+                    <CardContent
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            padding: '1.5rem'
+                        }}
+                    >
+                        <IconButton aria-label="email">
+                            <WhatsAppIcon />
+                        </IconButton>
+                        <Typography
+                            sx={{
+                                fontSize: '1.1rem',
+                                color: 'text.secondary',
+                                fontWeight: 'bold',
+                                ml: '1rem'
+                            }}
+                        >neamulhaque3298@gmail.com</Typography>
+                    </CardContent>
+                </Card>
             </Stack>
         </Box >
     )
